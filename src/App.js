f4f01b339b62ef57aedf972f93664414dad7d5e8
kalyaniRane <kalyanirane19@gmail.com>
2021-08-21 15:48:23 +0530

    ADDED : Merge The Footer Component On App Component.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
 import Content from './Content';
+import Footer from './Footer';
 
 function App() {
   return (
     <div className="App">
      <Header/>
      <Content/>
+     <Footer/>
     </div>
   );
 }
