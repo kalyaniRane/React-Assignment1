21f64dfbbbd75f0176697fa2b7ac7f4e3d67e6fb
kalyaniRane <kalyanirane19@gmail.com>
2021-08-21 15:46:42 +0530

    ADDED : Merge The Content Component On App Component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
+import Content from './Content';
 
 function App() {
   return (
     <div className="App">
      <Header/>
+     <Content/>
     </div>
   );
 }
