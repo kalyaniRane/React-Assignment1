33e7c6f87d877b77b07466318df6d0ba2784b6e7
kalyaniRane <kalyanirane19@gmail.com>
2021-08-21 15:44:03 +0530

    ADDED : Merge The Header Component On App Component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
-     <h1>Hello World</h1>
+     <Header/>
     </div>
   );
 }
